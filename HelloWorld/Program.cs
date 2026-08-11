57154ffa87e3c81031b0243d8d2d94031c4a4ca1
Przemek Szmajdziński <przemekszmajdzinski@MacBook-Pro-Przemek.local>
2026-08-11 13:57:08 +0200

    poprawa literowki z gita

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("To jest tekst z nowej gałęzi!");
 Console.WriteLine("Kolejny teest gałęzi");
-Console.WrtieLine("Test z githuba")
+Console.WrtieLine("Test z githuba");
